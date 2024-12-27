a656872fbb4a40f0233933ab6bf65cb4f54edbc8
QuanQuanQuai <quanda21@uef.edu.vn>
2024-12-27 18:47:05 +0700

    create dish screen, update dish model
    hi

--- src/models/dish.model.js
+++ src/models/dish.model.js
@@ -8,10 +8,10 @@ const DOCUMENT_NAME = 'Dish';
 const COLLECTION_NAME = 'Dishes';
 
 const dishSchema = new Schema({
-    name: { type: String, required: true },
+    name: { type: String , required: true, unique: true},
     description: { type: String, required: false },
     price: { type: Schema.Types.Decimal128, required: true },
-    category: { type: String, enum: ['starter', 'main dish', 'dessert'], required: true }
+    category: { type: String, enum: ['starter', 'main_dish', 'dessert'], required: true }
 }, {
     timestamps: true,          
     collection: COLLECTION_NAME
